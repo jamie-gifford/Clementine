c38d6f22d07fc0ccb9b1fa01c6286472b91a46e2
James Gifford <james@thoughtpatterns.com.au>
2016-07-23 09:35:42 +1000

    Revert "Remove support for mpris1"
    This reverts commit ed13d02231ba6b6cc1d02239edac59fb71b55750.

--- src/core/mpris.cpp
+++ src/core/mpris.cpp
@@ -18,12 +18,15 @@
 */
 
 #include "mpris.h"
+#include "mpris1.h"
 #include "mpris2.h"
 
 namespace mpris {
 
 Mpris::Mpris(Application* app, QObject* parent)
-    : QObject(parent), mpris2_(new mpris::Mpris2(app, this)) {
+    : QObject(parent),
+      mpris1_(new mpris::Mpris1(app, this)),
+      mpris2_(new mpris::Mpris2(app, mpris1_, this)) {
   connect(mpris2_, SIGNAL(RaiseMainWindow()), SIGNAL(RaiseMainWindow()));
 }
 
